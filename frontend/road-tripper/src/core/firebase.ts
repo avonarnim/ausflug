f10fef5056abb03193746f8df0bf4717a08c9727
Adam von Arnim <vonarnim@usc.edu>
2023-01-30 15:52:05 -0800

    fixing key usage

--- frontend/road-tripper/src/core/firebase.ts
+++ frontend/road-tripper/src/core/firebase.ts
@@ -18,14 +18,16 @@ import {
 } from "firebase/auth";
 export { FirebaseError, AuthErrorCodes, linkWithCredential };
 
+console.log(process.env);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB5_xjpUFYOrX81l7n1j4blpLuj8Dg7QUE",
-  authDomain: "road-tripper-376206.firebaseapp.com",
-  projectId: "road-tripper-376206",
-  storageBucket: "road-tripper-376206.appspot.com",
-  messagingSenderId: "44202708313",
-  appId: "1:44202708313:web:110f142aaaf7a97e9f1048",
-  measurementId: "G-XHJ1ZE1843",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- frontend/road-tripper/src/global.d.ts
+++ frontend/road-tripper/src/global.d.ts
@@ -4,3 +4,7 @@ declare const GOOGLE_CLOUD_PROJECT: string;
 declare const FIREBASE_APP_ID: string;
 declare const FIREBASE_API_KEY: string;
 declare const FIREBASE_AUTH_DOMAIN: string;
+declare const FIREBASE_PROJECT_ID: string;
+declare const FIREBASE_MESSAGING_SENDER_ID: string;
+declare const FIREBASE_MEASUREMENT_ID: string;
+declare const FIREBASE_STORAGE_BUCKET: string;
