d2cda0ea766a90804160b373a396c2a7c25f4cd7
Adam von Arnim <vonarnim@usc.edu>
2023-07-23 01:23:52 -0500

    ui: take up whole screen

--- frontend/onourway/src/App.tsx
+++ frontend/onourway/src/App.tsx
@@ -36,102 +36,113 @@ function App() {
   console.log("Theme", theme);
 
   return (
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <NavBar />
-        <Toolbar />
-        <Routes>
-          <Route index element={<React.Suspense children={<Home />} />} />
-          <Route
-            path="/privacy"
-            element={<React.Suspense children={<Privacy />} />}
-          />
-          <Route
-            path="/about"
-            element={<React.Suspense children={<About />} />}
-          />
-          <Route
-            path="/trips"
-            element={<React.Suspense children={<Trips />} />}
-          />
-          <Route
-            path="/trips/:oneWayRoundTrip/:origin/:originVal/:destination/:destinationVal/:startDate?/:endDate?"
-            element={<React.Suspense children={<NewTrip />} />}
-          />
-          <Route
-            path="/trips/:tripId"
-            element={<React.Suspense children={<EditTrip />} />}
-          />
-          <Route
-            path="/trips/random/:origin/:originVal"
-            element={<React.Suspense children={<RandomTrip />} />}
-          />
-          <Route
-            path="/spots/:spotId"
-            element={<React.Suspense children={<Spot />} />}
-          />
-          <Route
-            path="/addSpot"
-            element={<React.Suspense children={<AddSpot />} />}
-          />
-          <Route
-            path="/search"
-            element={<React.Suspense children={<Search />} />}
-          />
-          <Route
-            path="/activity"
-            element={<React.Suspense children={<Feed />} />}
-          />
-          <Route
-            path="/profile"
-            element={
-              <React.Suspense children={<Profile indicator={"none"} />} />
-            }
-          />
-          <Route
-            path="/profile/:userId"
-            element={<React.Suspense children={<Profile indicator={"id"} />} />}
-          />
-          <Route
-            path="/profile/u/:username"
-            element={
-              <React.Suspense children={<Profile indicator={"username"} />} />
-            }
-          />
-          <Route
-            path="/terms"
-            element={<React.Suspense children={<Terms />} />}
-          />
-          <Route
-            path="/settings"
-            element={<React.Suspense children={<Settings />} />}
-          />
-          <Route
-            path="/admin"
-            element={<React.Suspense children={<Admin />} />}
-          />
-          <Route
-            path="/register"
-            element={<React.Suspense children={<Register />} />}
-          />
-          <Route
-            path="/login"
-            element={<React.Suspense children={<Login />} />}
-          />
-          <Route path="/gas" element={<React.Suspense children={<Gas />} />} />
-          <Route
-            path="/guide/:name"
-            element={<React.Suspense children={<TripGuide />} />}
-          />
-          {/* <Route
+    <div className="app-wrapper">
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <NavBar />
+          <Toolbar />
+          <div className="routes-wrapper">
+            <Routes>
+              <Route index element={<React.Suspense children={<Home />} />} />
+              <Route
+                path="/privacy"
+                element={<React.Suspense children={<Privacy />} />}
+              />
+              <Route
+                path="/about"
+                element={<React.Suspense children={<About />} />}
+              />
+              <Route
+                path="/trips"
+                element={<React.Suspense children={<Trips />} />}
+              />
+              <Route
+                path="/trips/:oneWayRoundTrip/:origin/:originVal/:destination/:destinationVal/:startDate?/:endDate?"
+                element={<React.Suspense children={<NewTrip />} />}
+              />
+              <Route
+                path="/trips/:tripId"
+                element={<React.Suspense children={<EditTrip />} />}
+              />
+              <Route
+                path="/trips/random/:origin/:originVal"
+                element={<React.Suspense children={<RandomTrip />} />}
+              />
+              <Route
+                path="/spots/:spotId"
+                element={<React.Suspense children={<Spot />} />}
+              />
+              <Route
+                path="/addSpot"
+                element={<React.Suspense children={<AddSpot />} />}
+              />
+              <Route
+                path="/search"
+                element={<React.Suspense children={<Search />} />}
+              />
+              <Route
+                path="/activity"
+                element={<React.Suspense children={<Feed />} />}
+              />
+              <Route
+                path="/profile"
+                element={
+                  <React.Suspense children={<Profile indicator={"none"} />} />
+                }
+              />
+              <Route
+                path="/profile/:userId"
+                element={
+                  <React.Suspense children={<Profile indicator={"id"} />} />
+                }
+              />
+              <Route
+                path="/profile/u/:username"
+                element={
+                  <React.Suspense
+                    children={<Profile indicator={"username"} />}
+                  />
+                }
+              />
+              <Route
+                path="/terms"
+                element={<React.Suspense children={<Terms />} />}
+              />
+              <Route
+                path="/settings"
+                element={<React.Suspense children={<Settings />} />}
+              />
+              <Route
+                path="/admin"
+                element={<React.Suspense children={<Admin />} />}
+              />
+              <Route
+                path="/register"
+                element={<React.Suspense children={<Register />} />}
+              />
+              <Route
+                path="/login"
+                element={<React.Suspense children={<Login />} />}
+              />
+              <Route
+                path="/gas"
+                element={<React.Suspense children={<Gas />} />}
+              />
+              <Route
+                path="/guide/:name"
+                element={<React.Suspense children={<TripGuide />} />}
+              />
+              {/* <Route
             path="/donate"
             element={<React.Suspense children={<Donate />} />}
           /> */}
-        </Routes>
-        <Footer />
-      </ThemeProvider>
-    </AuthProvider>
+            </Routes>
+          </div>
+          <Footer />
+        </ThemeProvider>
+      </AuthProvider>
+    </div>
   );
 }
 
